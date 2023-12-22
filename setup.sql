a8f08e345d21cebc919d9c820d8358cef6c15c13
NguyenLongQuang <nguyenlongquang120103@gmail.com>
2023-12-22 10:48:44 +0700

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,4 +1,4 @@
-drop database ForestResources
+﻿drop database ForestResources
 go
 
 create database ForestResources
@@ -6,6 +6,7 @@ go
 
 use ForestResources
 go
+
 
 create table Facility
 ( Id int primary key identity(1, 1)
@@ -25,32 +26,79 @@ create table Offering
 )
 go
 
-insert into Facility (FormerName) values (N'GreenHarbor Woodworks')
-insert into Facility (FormerName) values (N'NatureGrow Nurseries')
-insert into Facility (FormerName) values (N'WildlifeCare Conservatories')
+create table Product
+( Id int primary key identity(1, 1)
+, ReferenceId int foreign key references Category(Id)
+, FormerName nvarchar(50)
+)
+go
 
-insert into Category (FormerName) values (N'Wood processing')
-insert into Category (FormerName) values (N'Seedling')
-insert into Category (FormerName) values (N'Creature management')
+create table ProductReport
+( Id int primary key identity(1, 1)
+, ReferenceId int foreign key references Product(Id)
+, Number int
+, RecordMonth int
+, RecordYear int
+)	
+go
+create table FacilityReport
+( Id int primary key identity(1, 1)
+, ReferenceId int foreign key references Facility(Id)
+, Number int
+, RecordMonth int
+, RecordYear int
+)	
+go
+
+create table BusinessStructure
+( Id int primary key identity(1, 1)
+, ReferenceId int foreign key references Facility(Id) 
+, FormerName nvarchar(50)
+)
+go
+
+create table ProductionMethod
+( Id int primary key identity(1, 1)
+, ReferenceId int foreign key references Facility(Id) 
+, FormerName nvarchar(50)
+)
+go
+
+create table District
+( Id int primary key identity(1, 1)
+, ReferenceId int foreign key references Facility(Id)
+, FormerName nvarchar(50)
+)
+go
+ 
+insert into Facility values (N'Cơ sở A')
+insert into Facility values (N'Cơ sở B')
+insert into Facility values (N'Cơ sở C')
+
+insert into Category values (N'Chế biến gỗ')
+insert into Category values (N'Cây giống')
+insert into Category values (N'Lưu trữ sinh vật rừng')
 
 insert into Offering (FacilityId, CatagoryId)
-select Id, (select Id from Category where FormerName = N'Wood processing')
+select Id, (select Id from Category where FormerName = N'Chế biến gỗ')
 from Facility
 where FormerName in 
-( N'GreenHarbor Woodworks'
+( N'Công ty B'
 )
 
 insert into Offering (FacilityId, CatagoryId)
-select Id, (select Id from Category where FormerName = N'Seedling')
+select Id, (select Id from Category where FormerName = N'Cây giống')
 from Facility
 where FormerName in 
-( N'NatureGrow Nurseries'
+( N'Công ty A'
+, N'Công ty B'
 )
 
 insert into Offering (FacilityId, CatagoryId)
-select Id, (select Id from Category where FormerName = N'Creature management')
+select Id, (select Id from Category where FormerName = N'Lưu trữ sinh vật rừng')
 from Facility
 where FormerName in 
-( N'WildlifeCare Conservatories'
+( N'Công ty C'
+, N'Công ty A'
 )
 
